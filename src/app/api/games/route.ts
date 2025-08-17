8cf6a8286978067b3fa5e98edaf5bda13ee78010
nils-nyberg <nilsnyberg24@hotmail.com>
2025-08-17 14:02:35 +0200

    Fix error message

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -22,6 +22,6 @@ export async function POST(req: NextRequest) {
     GAMES.push(game);
     return NextResponse.json({ wordLength, allowRepetition }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: "Error creating game" }, { status: 500 });
   }
 }
